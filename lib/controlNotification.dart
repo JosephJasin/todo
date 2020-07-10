baea3443655e80a48d967e1949509b600f5f1d6f
JosephJasin <josef20606@gmail.com>
2020-07-10 20:35:51 +0300

    Local Notifcations : adding control class headers only

---
+++ lib/controlNotification.dart
@@ -0,0 +1,29 @@
+///push notifications at a specific time and cancel them.
+class ControlNotification {
+
+  Future<void> pushNotificationAtSpecificTime({
+    String title = '',
+    String body = '',
+    int id = 0,
+
+  }) async {
+
+  }
+
+  Future<void> cancelNotification() async{
+
+  }
+
+  Future<void> cancelAllNotifications() async{
+
+  }
+
+  void debugPrintAllNotifications(){
+
+  }
+
+
+
+
+
+}
